9672e6f64e591a1f20a49917dc4146dbeb8d8e07
RASH1k-RAJ <pommyraj@gmail.com>
2023-01-09 22:39:08 +1100

    now mouse draws when clicked and moved

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 function createGrid(no_boxes){
     container.textContent = '';
     const box_width = CT_WIDTH/no_boxes;
-    console.log(`${box_width}`);
-
     for(let i = 0; i< no_boxes; i++){
         const row = document.createElement('div');
         row.style.display = 'flex';
@@ -15,16 +13,28 @@ function createGrid(no_boxes){
     }
 }
 
+function addDrew(event){
+    const a_box = event.target;
+    a_box.classList.add('drew');
+}
+
 function createBox(box_width){
     const a_box = document.createElement('div');
     a_box.classList.add('box');
     a_box.style.width = `${box_width}px`;
     a_box.style.height = `${box_width}px`;
-    a_box.addEventListener('mousemove', (e)=>{
-    a_box.classList.add('drew');
 
-            
-        })
+    a_box.addEventListener('mousedown', ()=>{
+        a_box.classList.add('drew');
+    });
+
+    container.addEventListener('mousedown', () =>{
+        a_box.addEventListener('mouseover', addDrew);
+        container.addEventListener('mouseup', ()=>{
+            a_box.removeEventListener('mouseover', addDrew)
+        });
+    });
+    
     return a_box;
 }
 
@@ -40,14 +50,17 @@ const bx_inpt = document.querySelector('.bx_inpt');
 const bx_inpt_bt = document.querySelector('.bx_inpt_bt');
 
 bx_inpt_bt.addEventListener('click', (e) =>{
-    if(!(bx_inpt.value <= 100 && bx_inpt.value >= 0)) return;
+    if(!(bx_inpt.value <= 100 && bx_inpt.value > 0)) return;
     no_boxes = bx_inpt.value
     createGrid(no_boxes);
     bx_inpt.value = ``;
 });
 
-const erase = document.querySelector('.clear');
-erase.addEventListener('click', () =>{
+const eraser_bt = document.querySelector('.eraser');
+
+const clear_bt = document.querySelector('.clear');
+clear_bt.addEventListener('click', () =>{
     createGrid(no_boxes);
 });
+
 
