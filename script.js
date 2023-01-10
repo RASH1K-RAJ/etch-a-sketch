ab48c1ef29d9e574e86091d5b0d64c2e095398d9
RASH1k-RAJ <pommyraj@gmail.com>
2023-01-10 11:18:02 +1100

    created color, eraser button and made changes in the basic logic of drawing.

--- script.js
+++ script.js
@@ -13,39 +13,67 @@ function createGrid(no_boxes){
     }
 }
 
-function addDrew(event){
-    const a_box = event.target;
-    a_box.classList.add('drew');
-}
-
 function createBox(box_width){
     const a_box = document.createElement('div');
     a_box.classList.add('box');
     a_box.style.width = `${box_width}px`;
     a_box.style.height = `${box_width}px`;
 
-    a_box.addEventListener('mousedown', ()=>{
-        a_box.classList.add('drew');
-    });
-
-    container.addEventListener('mousedown', () =>{
-        a_box.addEventListener('mouseover', addDrew);
-        container.addEventListener('mouseup', ()=>{
-            a_box.removeEventListener('mouseover', addDrew)
-        });
-    });
-    
     return a_box;
+}
+
+function addColor(event){
+    const a_box = event.target;
+    a_box.style['background-color'] = color_usd;
+}
+
+function clickAndMove(event){
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach((a_box) =>{
+        a_box.addEventListener('mouseover', addColor);
+    });
+}
+
+function removeClickAndMove(event){
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach((a_box) =>{
+        a_box.removeEventListener('mouseover', addColor);
+    });
 }
 
 const container = document.querySelector('.container');
 const container_style = window.getComputedStyle(container);
 const CT_WIDTH = parseInt(container_style.getPropertyValue('width'));
 
-createGrid(10);
+container.addEventListener('mousedown', clickAndMove);
+container.addEventListener('mouseup', removeClickAndMove);
+container.addEventListener('mouseleave', removeClickAndMove);
+
+//global variables
+let color_usd = 'black';
 var no_boxes = 10;
 
+createGrid(10);
 
+//radio buttons
+const options = document.querySelectorAll('.hide_bx');
+const color = document.querySelector('.color');
+const eraser = document.querySelector('.eraser');
+
+options.forEach((option)=>{
+    option.addEventListener("change", (event)=>{
+        color_usd = event.target.value;
+    });
+});
+
+
+//clear Button
+const clear_bt = document.querySelector('.clear');
+clear_bt.addEventListener('click', () =>{
+    createGrid(no_boxes);
+});
+
+//Submitting no of boxes
 const bx_inpt = document.querySelector('.bx_inpt');
 const bx_inpt_bt = document.querySelector('.bx_inpt_bt');
 
@@ -56,11 +84,5 @@ bx_inpt_bt.addEventListener('click', (e) =>{
     bx_inpt.value = ``;
 });
 
-const eraser_bt = document.querySelector('.eraser');
-
-const clear_bt = document.querySelector('.clear');
-clear_bt.addEventListener('click', () =>{
-    createGrid(no_boxes);
-});
 
 
